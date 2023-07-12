9cdd7e7d0db8319fd8058955472d271a08be94da
Spiru Andrei-Tudor <spiru.andrei1@gmail.com>
2023-07-12 14:20:29 +0100

    Added missing files

---
+++ src/components/charts/Donut.tsx
@@ -0,0 +1,49 @@
+import {
+    Chart,
+    ChartTitle,
+    ChartLegend,
+    ChartSeries,
+    ChartSeriesItem,
+    ChartSeriesLabels,
+  } from "@progress/kendo-react-charts";
+import { COLORS } from "../../constants";
+import { EnergyType } from "../Chart";
+  
+export const energyTypeDataExample = new Array<EnergyType>(
+    new EnergyType("Green", 150, COLORS.green),
+    new EnergyType("Fossil", 100, COLORS.orange),
+    new EnergyType("Nuclear", 75, COLORS.blue),
+    new EnergyType("Wind", 50, COLORS.blue)
+)
+  
+  // Show category label for each item in the donut graph
+  const labelContent = (e: { category: any; }) => e.category;
+
+interface Props {
+    energyData: EnergyType[];
+}
+  
+  const Donut: React.FC<Props> = ({ energyData } : Props) => {
+    return (
+      <Chart>
+        <ChartTitle text="Energy source distribution" />
+        <ChartLegend visible={false} />
+        <ChartSeries>
+          <ChartSeriesItem
+            type="donut"
+            data={energyData}
+            categoryField="name"
+            field="amount"
+          >
+            <ChartSeriesLabels
+              color="colour"
+              background="none"
+              content={labelContent}
+            />
+          </ChartSeriesItem>
+        </ChartSeries>
+      </Chart>
+    );
+  };
+  
+  export default Donut;
---
+++ src/constants.tsx
@@ -0,0 +1,6 @@
+export const COLORS = {
+    green: "#3CFF33",
+    orange: "#ff5833",
+    blue: "#3355ff",
+  };
+  
