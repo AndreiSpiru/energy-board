fcc176b9c15b438a4f4317bb81e3e73c1dcd5cb0
Jude Tyrrell <Jude.Tyrrell@softwire.com>
2023-07-12 14:17:19 +0100

    Chart component added

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import React from "react";
+import { useEffect, useState } from "react";
 
 export class EnergyType {
     name: string;
@@ -11,15 +12,38 @@ export class EnergyType {
         this.colour = colour;
     }
 }
-/*
+
+interface EnergyTypeJSON {
+    fuelType: string;
+    generation: number;
+}
+
+interface EnergyData {
+    startTime : string;
+    settlementPeriod: number;
+    data: Array<EnergyTypeJSON>;
+}
+
+
 const url = "https://data.dev.elexon.co.uk/bmrs/api/v1/generation/outturn/summary?from=2023-07-12&to=2023-07-12";
 
-export default function Chart ({ chart }) {
-    const [data, setData] = useState([]);
+
+export default function Chart (chart : React.FC) {
+    const [data, setData] = useState<EnergyData[]>([]);
+    const [types, setTypes] = useState<EnergyType[]>([]);
 
     function fetchInfo() {
         return fetch(url).then((res) => res.json()).then((res) => setData(res))
     }
 
+    useEffect(() => {
+        fetchInfo()
+    }, []);
 
-}*/
+    useEffect(() => {
+        let list = data[0].data;
+        setTypes(list.map((d) => new EnergyType(d["fuelType"], d["generation"], "#FFFFFF")));
+    }, [data]);
+
+    return chart(types);
+}
