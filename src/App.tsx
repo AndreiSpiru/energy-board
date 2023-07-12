20a4f12f00219fb8e30e682532644e17b3100c23
Spiru Andrei-Tudor <spiru.andrei1@gmail.com>
2023-07-12 11:41:38 +0100

    Implemented basic chart template

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "@progress/kendo-theme-material/dist/all.css";
+import "hammerjs";
+import "./App.css";
+import Donut from "./components/charts/Donut";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Build React Graphs The Easy Way</h1>
+        <div className="section">
+          <Donut />
+        </div>
+      </div>
     </div>
   );
 }
