15ad3de17888ac4347292ae2c01b4c5ea1c6018a
Jude Tyrrell <Jude.Tyrrell@softwire.com>
2023-07-13 14:39:26 +0100

    Prettier now

--- src/components/ChartDisplay.tsx
+++ src/components/ChartDisplay.tsx
@@ -35,15 +35,32 @@ const ChartDisplay : React.FC<Props> = () => {
     }
 
     return (<>
-        <select onChange={changeDataType}>
-            <option value={"generationType"}>Energy Generation Type</option>
-            <option value={"generationTypeOverTime"}>Energy Generation Type Over Time</option>
-        </select>
-        <select onChange={changeChartStyle}>
-            {dataType.supportedCharts.map(style => 
-                <option value={style}>{style}</option>)}
-        </select>
+        <ul className= "flex-container">
+            <label className="select" htmlFor="slct">
+                <select id="slct" onChange={changeDataType}>
+                    <option value={"generationType"}>Energy Generation Type</option>
+                    <option value={"generationTypeOverTime"}>Energy Generation Type Over Time</option>
+                </select>
+                <svg>
+                    <use xlinkHref="#select-arrow-down"></use>
+                </svg>
+            </label>
+            <label className="select" htmlFor="slct">
+                <select onChange={changeChartStyle}>
+                    {dataType.supportedCharts.map(style => 
+                        <option value={style}>{style}</option>)}
+                </select>
+                <svg>
+                    <use xlinkHref="#select-arrow-down"></use>
+                </svg>
+            </label>
+        </ul>
         <Chart dataType={dataType} chartStyle={chartStyle}/>
+        <svg className="sprites">
+            <symbol id="select-arrow-down" viewBox="0 0 10 6">
+                <polyline points="1 1 5 5 9 1"></polyline>
+            </symbol>
+        </svg>
         </>
     )
 }
--- src/constants.tsx
+++ src/constants.tsx
@@ -1,6 +1,8 @@
 export const COLORS = {
-    green: "#3CFF33",
-    orange: "#ff5833",
-    blue: "#3355ff",
-  };
+  green: "#3CFF33",
+  orange: "#ff5833",
+  blue: "#3355ff",
+  chartBg: "#D6DBDF",
+  pageBg: "#7E7E7E"
+};
   
