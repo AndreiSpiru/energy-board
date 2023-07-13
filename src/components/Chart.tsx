38d99f0af6fe3bd834c070f795794bff7a70aa56
Spiru Andrei-Tudor <spiru.andrei1@gmail.com>
2023-07-13 10:47:17 +0100

    Added initial implementation for over time api

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import GenerationTypeChart from "./GenerationType";
 import React from "react";
+import GenerationTypeOverTimeChart from "./GenerationTypeOverTime";
 
 export enum DataType {
-    generationType = "https://data.dev.elexon.co.uk/bmrs/api/v1/generation/outturn/summary?from=2023-07-12&to=2023-07-12"
+    generationType = "https://data.dev.elexon.co.uk/bmrs/api/v1/generation/outturn/summary?from=2023-07-12&to=2023-07-12",
+    generationTypeOverTime = "https://data.dev.elexon.co.uk/bmrs/api/v1/generation/outturn/summary"
 }
 
 export enum ChartStyle {
@@ -27,13 +29,17 @@ export interface SingleDimProps {
     data: SingleDimDataPoint[];
 }
 
-export interface TwoDimDataPoint {
+export class TwoDimDataPoint {
     label: string;
-    data: SingleDimDataPoint;
+    data:SingleDimDataPoint[];
+    constructor(label : string, data : SingleDimDataPoint[]) {
+        this.label = label;
+        this.data = data;
+    }
 }
 
 export interface TwoDimProps {
-    data: TwoDimDataPoint;
+    data: TwoDimDataPoint[];
 }
 
 interface Props {
@@ -57,6 +63,8 @@ const Chart : React.FC<Props> = (props : Props) => {
         switch (props.dataType) {
             case DataType.generationType:
                 return <GenerationTypeChart data={data} chartStyle={props.chartStyle}/>
+            case DataType.generationTypeOverTime:
+                return <GenerationTypeOverTimeChart data={data} chartStyle={props.chartStyle}/>
             default:
                 return (<></>)
         }
