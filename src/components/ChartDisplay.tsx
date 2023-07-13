accfd561b25d53683cd5217cac0de54c0c5d81b2
Spiru Andrei-Tudor <spiru.andrei1@gmail.com>
2023-07-13 16:43:31 +0100

    FIXED IT

--- src/components/ChartDisplay.tsx
+++ src/components/ChartDisplay.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Chart, { ChartStyle, DataType, DataTypes, getDataType } from "./Chart"
 
 interface Props {
@@ -18,13 +18,41 @@ function chartStyleFromString(s : string): ChartStyle {
     }
 }
 
+
+function getUrl(type: DataType): string {
+    switch(type.name){
+        case "generationTypeOverTime":
+        case "generationType":
+            return "https://data.dev.elexon.co.uk/bmrs/api/v1/generation/outturn/summary";
+        case "generationForecastOverTimeType":    
+        case "generationForecastType":
+            return "https://data.dev.elexon.co.uk/bmrs/api/v1/generation/availability/summary/14D";
+        case "generationForecastOverLongTimeType":
+        case "generationForecastLongType":
+            return "https://data.dev.elexon.co.uk/bmrs/api/v1/generation/availability/summary/3YW";
+        default:
+            return "";
+    }
+}
+
 const ChartDisplay : React.FC<Props> = () => {
     const [dataType, setDataType] = useState<DataType>(getDataType("generationType"));
     const [chartStyle, setChartStyle] = useState<ChartStyle>(ChartStyle.bar);
+    const [data, setData] = useState();
+
+    const fetchInfo = () => {
+        return fetch(getUrl(dataType)).then((res) => res.json()).then((res) => setData(res));
+    }
+
+    useEffect(() => {
+        fetchInfo()
+    }, [dataType]);
+
 
     function changeDataType(event : React.ChangeEvent<HTMLSelectElement>) {
         //console.log(event.target.value, getDataType(event.target.value))
         let newType = getDataType(event.target.value);
+        setData(undefined);
         setDataType(newType);
         setChartStyle(newType.supportedCharts[0]);
     }
@@ -55,7 +83,7 @@ const ChartDisplay : React.FC<Props> = () => {
                 </svg>
             </label>
         </ul>
-        <Chart dataType={dataType} chartStyle={chartStyle}/>
+        <Chart dataType={dataType} chartStyle={chartStyle} data={data}/>
         <svg className="sprites">
             <symbol id="select-arrow-down" viewBox="0 0 10 6">
                 <polyline points="1 1 5 5 9 1"></polyline>
